e6853ead3265da8263064edb3d7161d9a5435ee7
Keanu <kianenrico.loniakan@gmail.com>
2024-03-10 23:52:08 +0100

    rätt

--- startkod/js/JSFunctions.js
+++ startkod/js/JSFunctions.js
@@ -3,35 +3,87 @@
 
 
 
+let oGameData;
+
+initGlobalObject();
+
+function initGlobalObject(){
+
+    oGameData = {
+    
+        gameField : Array('', '', '', '', '', '', '', '', ''),
+        nickNamePlayerOne : document.getElementById('#nick1'),
+        nickNamePlayerTwo : document.getElementById('#nick2'),
+        playerOne : "X",
+        playerTwo : "O",
+        currentPlayer : "",
+        colorPlayerOne : '',
+        colorPlayerTwo : '',
+        timerEnabled : false,
+        timerId: null,
+        initGlobalObject: function(){},
+
+    
+        // funktion checkForGameOver med foor loops som rättar horizontel och vertikalt och även från höger till vänster
+        checkForGameOver: function(){
+           
+
+            
+            for (let raden = 0; raden < 3; raden++) {
+                if (this.gameField[raden * 3] === this.gameField[raden * 3 + 1] && this.gameField[raden * 3] === this.gameField[raden * 3 + 2] && this.gameField[raden * 3] !== '') { //rätta horizontel
+                    return this.gameField[raden * 3] === this.playerOne ? 1 : 2;
+                }
+            }
+
+                  //for loopen ovan med variabeln raden kollar om det är en vinst på det horizontella hållet av raden, när den hittar vinst visar den vinnande spe
+
+
+            for (let kolumner = 0; kolumner < 3; kolumner++) {
+                if (this.gameField[kolumner] === this.gameField[kolumner + 3] && this.gameField[kolumner] === this.gameField[kolumner + 6] && this.gameField[kolumner] !== '') { // rätta vertical
+
+                    return this.gameField[kolumner] === this.playerOne ? 1 : 2;                   
+                }
+            }
+                     //for loopen ovan med variabeln kolumner kollar om det är en vinst på det vertikala hållet av kolumnerna, när den hittar vinst visar den vinnande spe
+                  
+
+            if (this.gameField[0] === this.gameField[4] && this.gameField[0] === this.gameField[8] && this.gameField[0] !== '') {  // rätta diagonal från vänster till höger
+                return this.gameField[0] === this.playerOne ? 1 : 2;
+            }
+
+
+            
+            
+            if (this.gameField[2] === this.gameField[4] && this.gameField[2] === this.gameField[6] && this.gameField[2] !== '') { //rätta diagonal från höger till vänster
+                return this.gameField[2] === this.playerOne ? 1 : 2;
+            }
+
+            if (!this.gameField.includes('')) { 
+                return 3;  
+            }                                  // returnerar 3 om ingen vinner
+
+
+            return 0;                         // 0 blir det ifall ingen vinner
+
+
+        }
+    
+   
+
+    };
+
+    console.log(oGameData.currentPlayer);
+    console.log(oGameData.playerTwo);
+
+} 
+
+
+
+
 /* Koden nedan (onload) gör det möjligt att på uppstartningen
 av sidan så: Nickname1 redo att skrivas istället för att
 behöva klicka själv,
 */
-
-let oGameData;
-
-document.addEventListener("DOMContentLoaded",function(){
- oGameData = {
-    
-    gameField : Array('', '', '', '', '', '', '', '', ''),
-    nickNamePlayerOne : document.getElementById('#nick1'),
-    nickNamePlayerTwo : document.getElementById('#nick2'),
-   playerOne : "X",
-   playerTwo : "O",
-    currentPlayer : "",
-    colorPlayerOne : '',
-   colorPlayerTwo : '',
-   timerEnabled : false,
-   timerId: null 
-   
-
-};
-
-console.log(oGameData.currentPlayer);
-console.log(oGameData.playerTwo);
-
-}); 
-
 
 window.onload = function(){
     document.querySelector('#nick1').focus() 
@@ -39,99 +91,18 @@ window.onload = function(){
 };
 
 
-document.querySelector('#newGame').addEventListener('click',function(event){
-    
-
-    try {
-
-        let textRefs = this.querySelectorAll('input[type=text]');
-
-        let currentTextRef = null;
-
-        for( let counter =0; counter < textRefs.length; counter++){
-            currentTextRef = textRefs.item(counter);
-
-            if(currentTextRef.value.length === 0){
-
-                throw{elmRef : currentTextRef };
-            }
-        }
-
-
-        //färg väljare, updateras beronde på vad spelarna väljer
-        //hs1 funkar genom att kolla hs1(hue,saturation,lightness)
-        let color1Value = document.getElementById('color1').value;
-        let color2Value = document.getElementById('color2').value;
-
-        oGameData.colorPlayerOne = color1Value ? `hs1(${color1Value},100%, 50%` : `hs1(180, 100%, 50%)`
-        oGameData.colorPlayerTwo = color2Value ? `hs1(${color2Value},100%, 50%` : `hs1(180, 100%, 50%)`;
-
-
-        if (!oGameData.nickNamePlayerOne || !oGameData.nickNamePlayerTwo){
-            throw { errorMsg: 'glöm inte välja nicknames!!!'};
-
-        }
-
-
-
-        //iteration på ifall player one or two inte (!) eller är null skriv ut error
-        if (!oGameData.colorPlayerOne || !oGameData.colorPlayerTwo){
-            throw { errorMsg: 'glöm inte välja färger!!!'};
-        }
-
-        console.log("börja spela");
-
-        return false;
-    } 
-    
-    catch (oError) {
-
-        if(oError.elmRef) {
-            oError.elmRef.focus();
-            document.querySelector('#errorMsg').textContent = 'bruh glöm inte ' + oError.elmRef.getAttribute('placeHolder') + '!'; 
-        }
-
-        else {
-
-            document.querySelector('#errorMsg').textContent = oError.errorMsg;
-        }
-
-
-        return false;
-
-
-
-    }
-});
-
-
-
 
 const boxValue  = document.querySelectorAll('#game-area td')
 console.log(boxValue);
 
-/**
- * Globalt objekt som innehåller de attribut som ni skall använda.
- * Initieras genom anrop till funktionern initGlobalObject().
- */
 
-
-
-
-
-
-
+//De vinnande kominationerna gjort som 2d array vilket täcker horizontel,Vertical och båda diagonalerna
 const vinnandeKombinationer = [
-
-
-
-['X', 'X', 'X', '', '', '', '', '', ''],
-['X', '', '', 'X', '', '', 'X', '', ''],
-['X', '', '', '', 'X', '', '', '', 'X'],
-['', '', 'X', '', 'X', '', 'X', '', ''],
-['X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O']
-
-
+    ['X', 'X', 'X', '', '', '', '', '', ''],
+    ['X', '', '', 'X', '', '', 'X', '', ''],
+    ['X', '', '', '', 'X', '', '', '', 'X'],
+    ['', '', 'X', '', 'X', '', 'X', '', ''],
+    ['X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O']
 ];
 
 
@@ -139,97 +110,52 @@ initializeGame();
 
 function initializeGame(){
     boxValue.forEach(cell => cell.addEventListener("click", boxClick));
-   
-
 }
 
 function boxClick(event) {
-
+    // nedan för att bestämma spelare, ifall currentPlayer är playerOne retunera playerTwo annars ifall currentPlayer är tom retunera playerOne annars PlayerOne
+    oGameData.currentPlayer = oGameData.currentPlayer === oGameData.playerOne ? oGameData.playerTwo : oGameData.currentPlayer === "" ? oGameData.playerOne : oGameData.playerOne
+// deklarerar konstanten boxTarget som är event.target dvs att den riktar sig mot elementet som skapade händelsen
     const boxTarget = event.target;
+    if (boxTarget.innerText === ""){
+
+        
+        boxValue[boxTarget.id] = oGameData.currentPlayer;
+        oGameData.gameField[parseInt(boxTarget.getAttribute('data-id'))] = oGameData.currentPlayer;
+        boxTarget.innerText = oGameData.currentPlayer;
+
+        //else ifall boxen bär på värde redan
+    } else {
+        console.log("boxen bär redan värde!.")
+    }
+
+
+    //deklarerar konstanten vinnare som refererar till funktionen Spelarevann
+    const vinnare = oGameData.checkForGameOver();
+        
+    if(vinnare === 1 || vinnare === 2) { //ifall checkForGameOver retunerar 1 eller 2 (spelare 1 eller spelare 2) så console loggas respektiv siffra
+        console.log(vinnare);
+    }
+
+    else if (vinnare === 3){  //ifall ingen vinnare retuneras från checkForGameOver så console loggas siffran 3
+        console.log(vinnare)
+    }
+    
+
+    console.log("TEST:", boxValue)
+    console.log("TID:", boxTarget.getAttribute('data-id'))
+    console.log("ARRAY:", oGameData.gameField)
     console.log(boxTarget);
 
-
-if (!boxValue[boxTarget.id]){
-
-
-    const vinnare = playerHasWon();
-
-    if(vinnare) {
-        console.log(oGameData.currentPlayer + "vinst!");
-    }
-
-    else {
-
-        oGameData.currentPlayer = oGameData.currentPlayer === oGameData.playerOne ? oGameData.playerTwo : oGameData.playerOne;
-        
-    }
-
-    boxValue[boxTarget.id] = oGameData.currentPlayer;
-    boxTarget.innerText = oGameData.currentPlayer;
-
-
 }
 
 
 
- 
 
-
-}
-
-function playerHasWon(){
-    for (const condition of vinnandeKombinationer) {
-        let[a, b, c] = condition
-    
-        if(boxValue[a] && (boxValue[a]== boxValue[b] && boxValue[a] == boxValue[c])){
-            return[a,b,c]
-        }
-    }
-    return false
-    }
-
-
-
-
-
-
- let checkForGameOver = {
-
-    checkHorizontal,
-    checkVertical,
-    checkDiagonalLeftToRight,
-    checkDiagonalRightToLeft,
-
-
-
-}
-
- function checkHorizontal(){
-
-
-    if(['X', 'X', 'X', '', '', '', '', '', '']){
-
-
-    }
-
- }
-
- function checkVertical(){
-
- }
-
-function checkDiagonalLeftToRight(){
-
-}
-function checkDiagonalRightToLeft(){
-
-}
-
-
-function gameField(check){
+//function gameField(check){
 
     
-}
+//}
 
 /**
  * Kontrollerar för tre i rad.
@@ -241,11 +167,9 @@ function gameField(check){
  */
 
 
-oGameData.checkForGameOver = function(gameField) {
+//oGameData.checkForGameOver = function(gameField) {
 
-   
-
-}
+//}
 
 
 //Testutskrifter
@@ -276,7 +200,7 @@ console.log( oGameData.checkForDraw() );
 
     
     
-  //  oGameData.initGlobalObject = function() {
+//   oGameData.initGlobalObject = function() {
 
     //Datastruktur för vilka platser som är lediga respektive har brickor
    // oGameData.gameField = Array('', '', '', '', '', '', '', '', '');
@@ -288,11 +212,11 @@ console.log( oGameData.checkForDraw() );
     //oGameData.gameField = Array('', '', 'X', '', 'X', '', 'X', '', '');
     //oGameData.gameField = Array('X', 'O', 'X', '0', 'X', 'O', 'O', 'X', 'O');
 
-    //Indikerar tecknet som skall användas för spelare ett.
-   // oGameData.playerOne = "X";
+    // Indikerar tecknet som skall användas för spelare ett.
+    // oGameData.playerOne = "X";
 
-    //Indikerar tecknet som skall användas för spelare två.
-   // oGameData.playerTwo = "O";
+    // Indikerar tecknet som skall användas för spelare två.
+    // oGameData.playerTwo = "O";
 
     //Kan anta värdet X eller O och indikerar vilken spelare som för tillfället skall lägga sin "bricka".
    // oGameData.currentPlayer = "";
